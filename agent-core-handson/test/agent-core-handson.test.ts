22a2437f5ebabd5a36cb8025648840e7babde0aa
mu7889yoon <80621376+mu7889yoon@users.noreply.github.com>
2025-10-30 17:18:15 +0900

    cdk init

---
+++ agent-core-handson/test/agent-core-handson.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AgentCoreHandson from '../lib/agent-core-handson-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/agent-core-handson-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AgentCoreHandson.AgentCoreHandsonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
