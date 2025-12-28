c4618eaef5486dd941d8697c777e4e03882e03fd
mu7889yoon <80621376+mu7889yoon@users.noreply.github.com>
2025-12-29 01:50:41 +0900

    cdk init

---
+++ ecs-managed-instance-spot/iac/ecs-managed-instance-spot-cdk/test/ecs-managed-instance-spot-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsManagedInstanceSpotCdk from '../lib/ecs-managed-instance-spot-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-managed-instance-spot-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsManagedInstanceSpotCdk.EcsManagedInstanceSpotCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
